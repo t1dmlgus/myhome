af0099494b8b707c68428e74d97df2c06ba1f69e
t1dmlgus <dmlgusgngl@gmail.com>
2021-01-16 15:17:32 +0900

    글쓰기, 글수정

--- src/main/java/com/t1dmlgus/myhome/controller/BoardController.java
+++ src/main/java/com/t1dmlgus/myhome/controller/BoardController.java
@@ -1,15 +1,54 @@
 package com.t1dmlgus.myhome.controller;
 
+import com.t1dmlgus.myhome.model.Board;
+import com.t1dmlgus.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+
+        model.addAttribute("boards", boards);
+
         return "board/list";
     }
+
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+
+
+        if(id == null){
+            model.addAttribute("board", new Board());
+        } else{
+            Optional<Board> boardbyId = boardRepository.findById(id);
+            model.addAttribute("board",boardbyId);
+        }
+
+
+        return "board/form";
+
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute Board board){
+
+        boardRepository.save(board);
+
+        return "redirect:/board/list";
+    }
+
 }
---
+++ src/main/java/com/t1dmlgus/myhome/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.t1dmlgus.myhome.repository;
+
+import com.t1dmlgus.myhome.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+}
